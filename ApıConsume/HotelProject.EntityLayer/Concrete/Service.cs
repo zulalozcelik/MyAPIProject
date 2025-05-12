f165fa621e49138881ee928b2dbd8fa142bb3a79
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-05-12 13:26:17 +0300

    12.05.2025

--- ApıConsume/HotelProject.EntityLayer/Concrete/Service.cs
+++ ApıConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.EntityLayer.Concrete
 {
-    public class Services
+    public class Service
     {
         public int ServiceID { get; set; }
         public string ServiceIcon { get; set; }
