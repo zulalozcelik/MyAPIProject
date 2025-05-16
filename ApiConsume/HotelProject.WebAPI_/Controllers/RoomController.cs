208f004aa37185a08f794cb1b6881466145a3a4c
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-05-16 10:43:24 +0300

    16.05.2025

---
+++ ApiConsume/HotelProject.WebAPI_/Controllers/RoomController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebAPI_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+    }
+}
